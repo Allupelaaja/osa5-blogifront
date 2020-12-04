872eaf9019cc3b5594307df86606b261fbd3a230
Aleksi Heikkilä <aleksi.heikkila@metropolia.fi>
2020-12-04 16:45:00 +0200

    5.7 tehty

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleVisibility}>{visible === false ? 'view' : 'hide'}</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name === undefined ?
+          blog.user.username :
+          blog.user.name
+        }
+      </div>
+    </div>
+  )
+}
 
 export default Blog
